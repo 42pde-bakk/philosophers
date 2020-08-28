14cb7b47a30d75e45791cb15995930f5e8848348
Peer De bakker <pde-bakk@f1r4s9.codam.nl>
2020-08-28 17:02:47 +0200

    fixed when 2 philosophers would die

--- philo_three/manager.c
+++ philo_three/manager.c
@@ -6,7 +6,7 @@
 /*   By: pde-bakk <pde-bakk@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/17 20:39:21 by pde-bakk      #+#    #+#                 */
-/*   Updated: 2020/08/23 18:36:38 by pde-bakk      ########   odam.nl         */
+/*   Updated: 2020/08/28 17:01:51 by pde-bakk      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ void	genocide(t_data *data)
 
 	i = 0;
 	sem_wait(data->finished);
-	sem_wait(data->pen);
 	while (data->pids[i])
 	{
 		kill(data->pids[i], SIGTERM);
@@ -30,6 +29,17 @@ void	genocide(t_data *data)
 		waitpid(data->pids[i], NULL, 0);
 		++i;
 	}
+}
+
+void	philosopher_death(t_philo *phil)
+{
+	sem_wait(phil->data->pen);
+	ft_put_ul_fd(get_time_ms() - phil->data->starttime, 1);
+	ft_putchar_fd('\t', 1);
+	ft_put_ul_fd(phil->id, 1);
+	ft_putchar_fd(' ', 1);
+	ft_putstr_fd("has died", 1, 0);
+	ft_putchar_fd('\n', 1);
 }
 
 void	*mr_manager(void *param)
@@ -44,7 +54,7 @@ void	*mr_manager(void *param)
 		if (get_time_ms() - phil->last_ate >=
 			(unsigned long)phil->data->time_to_die)
 		{
-			philosopher_write(phil, "has died");
+			philosopher_death(phil);
 			sem_post(phil->data->finished);
 			return (0);
 		}
